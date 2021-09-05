8e09c0aaff99a2d15ddca73b14814631665edb32
Jarek Majka <vessekheal@gmail.com>
2021-09-05 15:53:44 +0200

    Skeleton plan added

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# First the program will take users input on his RPS choice
+def user_choice():
+    print('-' * 30)
+    print('1. Rock')
+    print('2. Paper')
+    print('3. Scissors')
+    chosen_value = int(input('Please choose one by entering a number corresponding to your choice: '))
 
 
-# Press the green button in the gutter to run the script.
+# Then it will generate random computers choice
+# Then it will compare both and choose a winner!
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    user_choice()
